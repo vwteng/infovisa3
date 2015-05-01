80d9c3a79c83ab305e1675abb8a191af3b4c9d83
chenjen <jennychen@Jennys-MacBook-Air.local>
2015-04-30 18:17:14 -0700

    no message

--- a3.js
+++ a3.js
@@ -1,30 +1,27 @@
 var margin = {top: 20, right: 20, bottom: 30, left: 50};
     var w = 960 - margin.left - margin.right;
-    var h = 500 - margin.top - margin.bottom;    
+    var h = 500 - margin.top - margin.bottom;
 
-var dataset;
+var dataset; //to hold full dataset
+// d3.csv("deathrates.csv", function(error, rates) {
+//   //read in the data
+//   if (error) return console.warn(error);
+//      stocks.forEach(function(d) {
+//         d.price = +d.price;
+//         d.tValue = +d.tValue;
+//         d.vol = +d.vol;
+//      });
+//    dataset=stocks;
+//    drawVis(dataset);
+// });
 
-d3.csv("deathrates.csv", function(error, rates) {
-  if (error) return console.warn(error);
-    rates.forEach(function(d) {
-      //console.log(d);
-      //d.code += d.code;
-      //d.region += d.region;
-      //for (i = 1960; i <= 2013; i++) {
-       // d.i += i;/
-    
-  });
 
-  dataset = rates;
-  drawVis(dataset);
-  //console.log(dataset);
-  //
-  //
-});
+// var col = d3.scale.category10();
 
-function drawVis(data) {
 
-var col = d3.scale.category10();
+// var colLightness = d3.scale.linear()
+//  .domain([0, 1200])
+//  .range(["#FFFFFF", "#000000"])
 
 var svg = d3.select("body").append("svg")
     .attr("width", w + margin.left + margin.right)
@@ -36,40 +33,51 @@ var x = d3.scale.linear()
         .domain([0, 1000])
         .range([0, w]);
 
-var xAxis = d3.svg.axis()
-        .scale(x)
-        .orient("bottom");
-
 var y = d3.scale.linear()
         .domain([0, 1000])
         .range([h, 0]);
 
+// function drawVis(data) {
+//   var circles = svg.selectAll("circle")
+//    .data(data)
+//    .enter()
+//    .append("circle")
+//     .attr("cx", function(d) { return x(d.price);  })
+//     .attr("cy", function(d) { return y(d.tValue);  })
+//     .attr("r", 4)
+//     .style("stroke", "black")
+//      //.style("fill", function(d) { return colLightness(d.vol); })
+//      .style("fill", function(d) { return col(d.type); })
+//     .style("opacity", 0.5)
+// }
+
+
+var xAxis = d3.svg.axis()
+    .scale(x);
+
+svg.append("g")
+    .attr("class", "axis")
+    .attr("transform", "translate(0," + h + ")")
+    .call(xAxis)
+     // .append("text")
+     //  .attr("x", w)
+     //  .attr("y", -6)
+     //  .style("text-anchor", "end")
+     //  .text("Price");
+
 var yAxis = d3.svg.axis()
-        .scale(y)
-        .orient("left");
+    .scale(y)
+    .orient("left");
 
-var circles = svg.selectAll("circle")
- .data(dataset)
- .enter()
- .append("circle")
-    .attr("cx", function(d) { return x(d.cat);  })
-    .attr("cy", function(d) { return y(d.dog);  })
-    .attr("r", 4)
-    .style("stroke", "red")
+svg.append("g")
+   .attr("class", "axis")
+   .call(yAxis)
+      // .append("text")
+      // .attr("transform", "rotate(-90)")
+      // .attr("y", 6)
+      // .attr("dy", ".71em")
+      // .style("text-anchor", "end")
+      // .text("True Value");
 
-    .on("mouseover", function(d) {
-      tooltip.transition()
-        .duration(200)
-        .style("opacity, 1");
-      tooltip.html("Stock" + d.name + ":" + d.price + "," + d.tValue + "," + d.vol)
-        .style("left", (d3.event.pageX + 5) + "px")
-        .style("top", (d3.event.pageY - 28) + "px")
-        .style("opacity", 1)
-    })
-    .on("mouseout", function(d) {
-      tooltip.transition()
-        .duration(500)
-        .style("opacity", 0);
-    })
-}
+
 
