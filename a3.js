1c0753fe426f96b10b5e10c574e5788a8fa24165
chenjen <jennychen@Jennys-MacBook-Air.local>
2015-04-30 21:11:50 -0700

    line graph of 3 countries

--- a3.js
+++ a3.js
@@ -1,41 +1,32 @@
 var margin = {top: 20, right: 20, bottom: 30, left: 50};
-    var w = 960 - margin.left - margin.right;
-    var h = 500 - margin.top - margin.bottom;
+    width = 960 - margin.left - margin.right;
+    height = 500 - margin.top - margin.bottom;
 
-var dataset; //to hold full dataset
-// d3.csv("deathrates.csv", function(error, rates) {
-//   //read in the data
-//   if (error) return console.warn(error);
-//      stocks.forEach(function(d) {
-//         d.price = +d.price;
-//         d.tValue = +d.tValue;
-//         d.vol = +d.vol;
-//      });
-//    dataset=stocks;
-//    drawVis(dataset);
-// });
+var parseDate = d3.time.format("%Y").parse;
 
+d3.csv("deathrates1.csv", function(error, data) {
+    data.forEach(function(d) {
+        d.year = parseDate(d.year);
+        d.death = +d.death;
+    });
 
-// var col = d3.scale.category10();
+var color = d3.scale.category10(); 
 
-
-// var colLightness = d3.scale.linear()
-//  .domain([0, 1200])
-//  .range(["#FFFFFF", "#000000"])
+var colLightness = d3.scale.linear()
+ .domain([0, 1200])
+ .range(["#FFFFFF", "#000000"])
 
 var svg = d3.select("body").append("svg")
-    .attr("width", w + margin.left + margin.right)
-    .attr("height", h + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform","translate(" + margin.left + "," + margin.top + ")");
 
-var x = d3.scale.linear()
-        .domain([0, 1000])
-        .range([0, w]);
+var x = d3.time.scale()
+    .range([0, width]);
 
 var y = d3.scale.linear()
-        .domain([0, 1000])
-        .range([h, 0]);
+    .range([height, 0]);
 
 // function drawVis(data) {
 //   var circles = svg.selectAll("circle")
@@ -51,33 +42,64 @@ var y = d3.scale.linear()
 //     .style("opacity", 0.5)
 // }
 
+var trendline = d3.svg.line()   
+    .x(function(d) { 
+        return x(d.year); 
+    })
+    .y(function(d) { 
+        return y(d.death); 
+    });
+    
+    // Scale the range of the data
+    x.domain(d3.extent(data, function(d) { 
+        return d.year; 
+    }));
+    y.domain([0, d3.max(data, function(d) { 
+        return d.death; 
+    })]);
 
-var xAxis = d3.svg.axis()
-    .scale(x);
+    // Nest the entries by country
+    var dataNest = d3.nest()
+        .key(function(d) {
+            return d.country;
+        })
+        .entries(data);
 
-svg.append("g")
-    .attr("class", "axis")
-    .attr("transform", "translate(0," + h + ")")
-    .call(xAxis)
-     // .append("text")
-     //  .attr("x", w)
-     //  .attr("y", -6)
-     //  .style("text-anchor", "end")
-     //  .text("Price");
+    // Loop through each country
+    dataNest.forEach(function(d,i) { 
 
-var yAxis = d3.svg.axis()
+        svg.append("path")
+            .attr("class", "line")
+            .style("stroke", function() { // Add the colours dynamically
+                return d.color = color(d.key); })
+            .attr("d", trendline(d.values));
+    });
+
+    var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+        // .append("text")
+         //  .attr("x", w)
+         //  .attr("y", -6)
+         //  .style("text-anchor", "end")
+         //  .text("Price");
+
+    var yAxis = d3.svg.axis()
     .scale(y)
     .orient("left");
 
-svg.append("g")
-   .attr("class", "axis")
-   .call(yAxis)
-      // .append("text")
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis);
+        // .append("text")
       // .attr("transform", "rotate(-90)")
       // .attr("y", 6)
       // .attr("dy", ".71em")
       // .style("text-anchor", "end")
       // .text("True Value");
-
-
-
+});
